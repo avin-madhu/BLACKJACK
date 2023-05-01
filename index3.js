4d97232ee6dd9044d762de9f18c86b5c7e380fc8
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2023-05-01 17:07:10 +0530

    click and sfx added

--- index3.js
+++ index3.js
@@ -10,10 +10,28 @@ let cashWithdrawn=document.getElementById("CashWithDrawn")
 let cashNow =0
 let cashSum =0
 let WonMessage = document.getElementById("marqueeTag")
-
 let messageEl=document.getElementById("message-el")
 let sumEl=document.getElementById("sum-el")
 let cardsEl=document.getElementById("cards-el")
+let clickSound = new Audio("click.mp3");
+let winSound = new Audio("win.mp3");
+let loseSound = new Audio("lose.mp3");
+let startButton = document.getElementById("Startgame-Button")
+let replayButton = document.getElementById("Replay-Button")
+let newCardButton = document.getElementById("newcard-btn")
+
+
+startButton.addEventListener('click',function(){
+  clickSound.play();
+})
+
+newCardButton.addEventListener('click',function(){
+  clickSound.play();
+})
+
+replayButton.addEventListener('click',function(){
+  clickSound.play();
+})
 
 function getrandomcard(){
     return Math.floor(Math.random()*14)
@@ -59,7 +77,10 @@ function openNav() {
   }
 
 
-function rendergame(){
+  
+  function rendergame(){
+   if(isalive==true)
+   {
     cardsEl.textContent="Cards: "
     sumEl.textContent= "Sum: "+sum
 
@@ -67,42 +88,47 @@ function rendergame(){
     {
       cardsEl.textContent+=cardsarray[i]+" "
     }
+   }
 
 if (sum<=20){
     message="click NEW CARD to draw a card"
 }else if (sum===21){
   messageEl.style.color="#11fa04"
-    message="BLACKJACK!!"
+    message="BLACKJACK 🎉 ";
     won = true
 }else{
     messageEl.style.color="red"
-    message="You Lost :("
+    message="You Lost 😢 " 
     isalive=false
+    cashNow=0;
+    cashSum=0;
 }
  messageEl.textContent=message
 
 }
 
-function newcard(){
+
+if(isalive==true)
+{
+  function newcard(){
 
   
-   if(startclicked==1)
-   {
-    let thirdcard= getrandomcard()
-   sum+=thirdcard
-   cardsarray.push(thirdcard)
-   rendergame()
-   if(isalive==true)
-   {
-    cashUpdate()
-   }
-   else
-   {
-    WonMessage.style.color="black"
-   }
-   }
-  
-  
+    if(startclicked==1)
+    {
+     let thirdcard= getrandomcard()
+    sum+=thirdcard
+    cardsarray.push(thirdcard)
+    rendergame()
+    if(isalive==true)
+    {
+     cashUpdate()
+    }
+    else
+    {
+     WonMessage.style.color="black"
+    }
+    }
+ }
 }
 
 function cashUpdate()
